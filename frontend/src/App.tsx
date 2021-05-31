e7eaa76dbb9cea176cb8e554d6c204266e9efe75
Aaron <eusahn@icloud.com>
2021-05-30 17:18:49 -0700

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-blue-300">
+      Yes sir
+    </div>
+  );
+}
+
+export default App;
